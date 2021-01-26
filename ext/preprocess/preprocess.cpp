015169997fabb25bc4640db911c57408202f0e6c
Shengjie Lin <StArchon@outlook.com>
2021-01-26 01:24:14 -0600

    added support to count pixelwise

--- ext/preprocess/preprocess.cpp
+++ ext/preprocess/preprocess.cpp
@@ -115,8 +115,10 @@ py::array_t<int> count_nbs(array_f32_d tgt_dm_np, array_f32_d tgt_K_np,
                 float proj_d = src_uvd(2);
                 float src_wf = src_uvd(0) / proj_d;
                 float src_hf = src_uvd(1) / proj_d;
-                int src_w = int(src_wf);
-                int src_h = int(src_hf);
+                // int src_w = int(src_wf);
+                // int src_h = int(src_hf);
+                int src_w = int(src_wf - 0.5);
+                int src_h = int(src_hf - 0.5);
 
                 if (is_valid_projection(src_h, src_w, proj_d, src_dms[vidx],
                                         height, width, bwd_depth_thresh,
@@ -128,6 +130,113 @@ py::array_t<int> count_nbs(array_f32_d tgt_dm_np, array_f32_d tgt_K_np,
     }
 
     return create_array1<int>(count);
+}
+
+// For each pixel of the target map, count the number of views that also observe that pixel
+py::array_t<int> count_nbs_per_pix(array_f32_d tgt_dm_np, array_f32_d tgt_K_np,
+                           array_f32_d tgt_R_np, array_f32_d tgt_t_np,
+                           array_f32_d src_dms_np, array_f32_d src_Ks_np,
+                           array_f32_d src_Rs_np, array_f32_d src_ts_np,
+                           float bwd_depth_thresh) {
+    if (tgt_dm_np.ndim() != 2) {
+        throw std::invalid_argument("tgt_dm has to be height x width");
+    }
+    int height = tgt_dm_np.shape(0);
+    int width = tgt_dm_np.shape(1);
+    if (tgt_K_np.ndim() != 2 || tgt_K_np.shape(0) != 3 ||
+        tgt_K_np.shape(1) != 3) {
+        throw std::invalid_argument("tgt_K has to be 3 x 3");
+    }
+    if (tgt_R_np.ndim() != 2 || tgt_R_np.shape(0) != 3 ||
+        tgt_R_np.shape(1) != 3) {
+        throw std::invalid_argument("tgt_R has to be 3 x 3");
+    }
+    if (tgt_t_np.ndim() != 1 || tgt_R_np.shape(0) != 3) {
+        throw std::invalid_argument("tgt_R has to be 3");
+    }
+    if (src_dms_np.ndim() != 3 || src_dms_np.shape(1) != height ||
+        src_dms_np.shape(2) != width) {
+        throw std::invalid_argument(
+            "src_dms has to be n_views x height x width");
+    }
+    int n_views = src_dms_np.shape(0);
+    if (src_Ks_np.ndim() != 3 || src_Ks_np.shape(0) != n_views ||
+        src_Ks_np.shape(1) != 3 || src_Ks_np.shape(2) != 3) {
+        throw std::invalid_argument("Ks has to be n_views x 3 x 3");
+    }
+    if (src_Rs_np.ndim() != 3 || src_Rs_np.shape(0) != n_views ||
+        src_Rs_np.shape(1) != 3 || src_Rs_np.shape(2) != 3) {
+        throw std::invalid_argument("Rs has to be n_views x 3 x 3");
+    }
+    if (src_ts_np.ndim() != 2 || src_ts_np.shape(0) != n_views ||
+        src_ts_np.shape(1) != 3) {
+        throw std::invalid_argument("ts has to be n_views x 3");
+    }
+
+    mat3_t tgt_K(tgt_K_np.data(), 3, 3);
+    mat3_t tgt_R(tgt_R_np.data(), 3, 3);
+    vec3_t tgt_t(tgt_t_np.data(), 3, 1);
+    proj_t tgt_Pi;  // the inverse projection, i.e. frame coord to world ray
+    tgt_Pi.leftCols<3>() = tgt_K.inverse();
+    tgt_Pi.rightCols<1>() = -tgt_t;
+    tgt_Pi = tgt_R.transpose() * tgt_Pi;
+
+    depthmap_t tgt_dm(tgt_dm_np.data(), height, width);
+
+    std::vector<proj_t> src_Ps;
+    std::vector<Eigen::Vector3f> src_Cs;
+    for (int vidx = 0; vidx < n_views; ++vidx) {
+        mat3_t K(src_Ks_np.data() + vidx * 3 * 3, 3, 3);
+        mat3_t R(src_Rs_np.data() + vidx * 3 * 3, 3, 3);
+        vec3_t t(src_ts_np.data() + vidx * 3 * 1, 3, 1);
+        proj_t P;
+        P.leftCols<3>() = R;
+        P.rightCols<1>() = t;
+        P = K * P;
+        src_Ps.push_back(P);
+        Eigen::Vector3f C = -R.transpose() * t;
+        src_Cs.push_back(C);
+    }
+
+    std::vector<depthmap_t> src_dms;
+    for (int vidx = 0; vidx < n_views; ++vidx) {
+        src_dms.push_back(depthmap_t(src_dms_np.data() + vidx * height * width,
+                                     height, width));
+    }
+
+    std::vector<int> count(height * width, 0);
+    for (int tgt_h = 0; tgt_h < height; ++tgt_h) {
+        for (int tgt_w = 0; tgt_w < width; ++tgt_w) {
+            float dt = tgt_dm(tgt_h, tgt_w);
+            if (dt <= 0) {
+                continue;
+            }
+
+            Eigen::Vector4f tgt_uvd(dt * (float(tgt_w) + 0.5),
+                                    dt * (float(tgt_h) + 0.5), dt, 1);
+            Eigen::Vector3f xyz = tgt_Pi * tgt_uvd;
+            Eigen::Vector4f xyzh(xyz(0), xyz(1), xyz(2), 1);
+
+            for (int vidx = 0; vidx < n_views; ++vidx) {
+                Eigen::Vector3f src_uvd = src_Ps[vidx] * xyzh;
+                float proj_d = src_uvd(2);
+                float src_wf = src_uvd(0) / proj_d;
+                float src_hf = src_uvd(1) / proj_d;
+                // int src_w = int(src_wf);
+                // int src_h = int(src_hf);
+                int src_w = int(src_wf - 0.5);
+                int src_h = int(src_hf - 0.5);
+
+                if (is_valid_projection(src_h, src_w, proj_d, src_dms[vidx],
+                                        height, width, bwd_depth_thresh,
+                                        false)) {
+                    count[tgt_h*width+tgt_w]++;
+                }
+            }
+        }
+    }
+
+    return create_array2<int>(count, height, width);
 }
 
 std::tuple<py::array_t<float>, py::array_t<float>, py::array_t<float>>
--- ext/preprocess/preprocess.h
+++ ext/preprocess/preprocess.h
@@ -8,6 +8,12 @@ py::array_t<int> count_nbs(array_f32_d tgt_dm_np, array_f32_d tgt_K_np,
                            array_f32_d src_Rs_np, array_f32_d src_ts_np,
                            float bwd_depth_thresh);
 
+py::array_t<int> count_nbs_per_pix(array_f32_d tgt_dm_np, array_f32_d tgt_K_np,
+                           array_f32_d tgt_R_np, array_f32_d tgt_t_np,
+                           array_f32_d src_dms_np, array_f32_d src_Ks_np,
+                           array_f32_d src_Rs_np, array_f32_d src_ts_np,
+                           float bwd_depth_thresh);
+
 std::tuple<py::array_t<float>, py::array_t<float>, py::array_t<float>>
 get_sampling_map(array_f32_d tgt_dm_np, array_f32_d tgt_K_np,
                  array_f32_d tgt_R_np, array_f32_d tgt_t_np,
